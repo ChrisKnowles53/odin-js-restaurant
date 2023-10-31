880044b475d32faccbd4fe5ae2afbbef1f8323b9
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-10-31 15:35:55 +0000

    private: true added and paageLoad refactored

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,9 +1,73 @@
+// Option1 - original code
+// import hangingSpiderman from "../images/hanging-spiderman.png";
+// import backgroundImage from "../images/background-web.jpeg";
+
+// function createElementWithText(elemType, textContent) {
+//   const element = document.createElement(elemType);
+//   element.textContent = textContent;
+//   return element;
+// }
+
+// export default function createPage() {
+//   const html = document.querySelector("html");
+//   html.style.backgroundImage = `url(${backgroundImage}`;
+
+//   const div = document.getElementById("content");
+
+//   const titleElement = createElementWithText("h1", "The Spiders Web");
+//   const descriptionElement = createElementWithText(
+//     "p",
+//     "What delights do you think we eat here? 🪰flies🪰"
+//   );
+//   descriptionElement.setAttribute("class", "page-load-paragraph");
+
+//   const spidermanImage = document.createElement("img");
+//   spidermanImage.setAttribute("src", hangingSpiderman);
+//   spidermanImage.setAttribute("alt", "spiderweb image");
+
+//   div.appendChild(titleElement);
+//   div.appendChild(descriptionElement);
+//   div.appendChild(spidermanImage);
+// }
+
+// function createHeader() {
+//   const div = document.getElementById("content");
+//   const body = document.querySelector("body");
+//   const header = createElementWithText("header");
+//   header.setAttribute("id", "header");
+//   header.setAttribute("class", "header");
+
+//   const menuButton = createElementWithText("button", "Menu");
+//   menuButton.setAttribute("id", "menu-button");
+//   menuButton.setAttribute("class", "menu-button");
+
+//   const contentButton = createElementWithText("button", "Contact");
+//   contentButton.setAttribute("id", "contact-button");
+//   contentButton.setAttribute("class", "contact-button");
+
+//   const homeButton = createElementWithText("button", "Home");
+//   homeButton.setAttribute("id", "home-button");
+//   homeButton.setAttribute("class", "home-button");
+
+//   header.appendChild(homeButton);
+//   header.appendChild(menuButton);
+//   header.appendChild(contentButton);
+//   body.insertBefore(header, div);
+
+//   homeButton.classList.add("active-button");
+// }
+
+// export { createHeader };
+
+// Option2 - cleaner code attempt
 import hangingSpiderman from "../images/hanging-spiderman.png";
 import backgroundImage from "../images/background-web.jpeg";
 
-function createElementWithText(elemType, textContent) {
-  const element = document.createElement(elemType);
+function createMyElement(elementType, textContent, id, classname) {
+  const element = document.createElement(elementType);
   element.textContent = textContent;
+  if (id) element.setAttribute("id", id);
+  if (classname) element.setAttribute("class", classname);
   return element;
 }
 
@@ -13,12 +77,13 @@ export default function createPage() {
 
   const div = document.getElementById("content");
 
-  const titleElement = createElementWithText("h1", "The Spiders Web");
-  const descriptionElement = createElementWithText(
+  const titleElement = createMyElement("h1", "The Spiders Web");
+  const descriptionElement = createMyElement(
     "p",
-    "What delights do you think we eat here? 🪰flies🪰"
+    "What delights do you think we eat here? 🪰flies🪰",
+    "description",
+    "page-load-paragraph"
   );
-  descriptionElement.setAttribute("class", "page-load-paragraph");
 
   const spidermanImage = document.createElement("img");
   spidermanImage.setAttribute("src", hangingSpiderman);
@@ -32,21 +97,26 @@ export default function createPage() {
 function createHeader() {
   const div = document.getElementById("content");
   const body = document.querySelector("body");
-  const header = createElementWithText("header");
-  header.setAttribute("id", "header");
-  header.setAttribute("class", "header");
+  const header = createMyElement("header", "", "header", "header");
 
-  const menuButton = createElementWithText("button", "Menu");
-  menuButton.setAttribute("id", "menu-button");
-  menuButton.setAttribute("class", "menu-button");
-
-  const contentButton = createElementWithText("button", "Contact");
-  contentButton.setAttribute("id", "contact-button");
-  contentButton.setAttribute("class", "contact-button");
-
-  const homeButton = createElementWithText("button", "Home");
-  homeButton.setAttribute("id", "home-button");
-  homeButton.setAttribute("class", "home-button");
+  const menuButton = createMyElement(
+    "button",
+    "Menu",
+    "menu-button",
+    "menu-button"
+  );
+  const contentButton = createMyElement(
+    "button",
+    "Contact",
+    "contact-button",
+    "contact-button"
+  );
+  const homeButton = createMyElement(
+    "button",
+    "Home",
+    "home-button",
+    "home-button"
+  );
 
   header.appendChild(homeButton);
   header.appendChild(menuButton);
